dc3013a3a9ff90caa98a64e3dac122f34e9dfa9e
Gabriel Souza <92094301+gabrielsl11@users.noreply.github.com>
2024-10-24 15:34:28 -0300

    Update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,53 +1,62 @@
 var desserts = [
   {
+    id: 1,
     image: "assets/images/image-waffle-thumbnail.jpg",
     subtitle: "Waffle",
     title: "Waffle with Berries",
     price: 6.5,
   },
   {
+    id: 2,
     image: "assets/images/image-creme-brulee-thumbnail.jpg",
     subtitle: "Crème Brúlée",
     title: "Vanilla Bean Crème Brûlée",
     price: 7,
   },
   {
+    id: 3,
     image: "assets/images/image-macaron-thumbnail.jpg",
     subtitle: "Macaron",
     title: "Macaron Mix of Five",
     price: 8,
   },
   {
+    id: 4,
     image: "assets/images/image-tiramisu-thumbnail.jpg",
     subtitle: "Tiramisu",
     title: "Classic Tiramisu",
     price: 5.5,
   },
   {
+    id: 5,
     image: "assets/images/image-baklava-thumbnail.jpg",
     subtitle: "Baklava",
     title: "Pistachio Baklava",
     price: 4,
   },
   {
+    id: 6,
     image: "assets/images/image-meringue-thumbnail.jpg",
     subtitle: "Pie",
     title: "Lemon Meringue Pie",
     price: 6,
   },
   {
+    id: 7,
     image: "assets/images/image-cake-thumbnail.jpg",
     subtitle: "Cake",
     title: "Red Valvet Cake",
     price: 4.5,
   },
   {
+    id: 8,
     image: "assets/images/image-brownie-thumbnail.jpg",
     subtitle: "Brownie",
     title: "Salted Caramel Brownie",
     price: 4.5,
   },
   {
+    id: 9,
     image: "assets/images/image-panna-cotta-thumbnail.jpg",
     subtitle: "Panna Cotta",
     title: "Vanilla Panna Cotta",
@@ -65,9 +74,16 @@ document.addEventListener("DOMContentLoaded", () => {
     gridItem.innerHTML = `
           <div class="img-item">
             <img src="${desserts[i].image}" alt="${desserts[i].title}" />
-            <span>
+            
+            <span id="${desserts[i].id}" class="select-item">
               <i class="bi bi-cart"></i>  
               Add to Cart
+            </span>
+
+            <span class="add-to-cart hidden">
+              <i id="addOne" class="bi bi-dash-circle"></i>
+              0
+              <i id="removeOne" class="bi bi-plus-circle"></i>
             </span>
           </div>
           <div class="desc-item">
@@ -79,4 +95,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     grid.appendChild(gridItem);
   }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".select-item").forEach((item) => {
+    item.addEventListener("click", (event) => {
+
+      const clickedItem = event.target;
+
+      clickedItem.classList.add("hidden");
+
+      const addToCart = clickedItem.nextElementSibling;
+
+      if (addToCart) {
+        addToCart.classList.remove("hidden");
+      }
+    });
+  });
 });
